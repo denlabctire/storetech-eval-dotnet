4f100197f91c4299107a0d078ed21a920c766585
Denis Labelle <denis.labelle@cantire.com>
2026-03-26 18:04:29 -0600

    Removed unused usings

--- Services/CartService.cs
+++ Services/CartService.cs
@@ -1,16 +1,10 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using storetech_eval_dotnet.Contracts.Cart;
-using storetech_eval_dotnet.Data;
-using storetech_eval_dotnet.Domain;
 using storetech_eval_dotnet.Options;
 
 namespace storetech_eval_dotnet.Services;
 
 public sealed class CartService(
-    StoreTechDbContext dbContext,
-    IProductService productService,
-    ITaxService taxService,
     IOptions<CartOptions> cartOptions) : ICartService
 {
     private readonly CartOptions _cartOptions = cartOptions.Value;
